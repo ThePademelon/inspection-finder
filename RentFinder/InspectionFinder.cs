20c84e9993cbfaf30e9b2383df3fe36e701a6e1c
ThePademelon <zomanom@gmail.com>
2022-04-11 00:22:43 +1000

    Scrape description data

--- RentFinder/InspectionFinder.cs
+++ RentFinder/InspectionFinder.cs
@@ -1,5 +1,6 @@
 using System.Globalization;
 using System.IO.Compression;
+using System.Text.Json.Nodes;
 using System.Text.RegularExpressions;
 using HtmlAgilityPack;
 
@@ -7,6 +8,21 @@ namespace RentFinder;
 
 public static class InspectionFinder
 {
+    private static readonly HttpClient HttpClient;
+
+    static InspectionFinder()
+    {
+        HttpClient = new HttpClient
+        {
+            DefaultRequestHeaders =
+            {
+                {"User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0"},
+                {"Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"},
+                {"Accept-Encoding", "gzip, deflate, br"}
+            },
+        };
+    }
+
     public static async Task Search(Options options)
     {
         var page = 1;
@@ -21,34 +37,34 @@ public static class InspectionFinder
             @continue = data.Any();
             foreach (var listing in data)
             {
-                Console.WriteLine($"Address:        {listing.Location}");
-                Console.WriteLine($"Beds:           {listing.Beds}");
-                Console.WriteLine($"Rent:           {listing.Price:$0.00}");
-                Console.WriteLine($"Rent per Bed:   {listing.PricePerBed:$0.00}");
+                var list = await listing;
+                Console.WriteLine($"Address:            {list.Location}");
+                Console.WriteLine($"Beds:               {list.Beds}");
+                Console.WriteLine($"Rent:               {list.Price:$0.00}");
+                Console.WriteLine($"Rent per Bed:       {list.PricePerBed:$0.00}");
+                Console.WriteLine($"Air Conditioning:   {(list.AirCon ? '✅' : '❓')}");
                 Console.WriteLine(horizontalRule);
             }
         } while (@continue);
     }
 
-    private static async Task<IEnumerable<Listing>> DeserializePageData(string url)
+    private static async Task<IEnumerable<Task<Listing>>> DeserializePageData(string url)
     {
-        var stream = await new HttpClient
-        {
-            DefaultRequestHeaders =
-            {
-                {"User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0"},
-                {"Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"},
-                {"Accept-Encoding", "gzip, deflate, br"}
-            },
-        }.GetStreamAsync(url);
-        var web = new HtmlDocument();
-        web.Load(new GZipStream(stream, CompressionMode.Decompress));
+        var web = await GetDocument(url);
         return web.DocumentNode.Descendants("div")
             .Where(x => x.HasMatchingDataId("listing-card-inspection"))
             .Select(ConvertToListing);
     }
 
-    private static Listing ConvertToListing(HtmlNode listingNode)
+    private static async Task<HtmlDocument> GetDocument(string url)
+    {
+        var stream = await HttpClient.GetStreamAsync(url);
+        var web = new HtmlDocument();
+        web.Load(new GZipStream(stream, CompressionMode.Decompress));
+        return web;
+    }
+
+    private static async Task<Listing> ConvertToListing(HtmlNode listingNode)
     {
         var bedsRegex = new Regex(@"(\d+) Beds?");
         var bedsText = listingNode.Descendants("span")
@@ -65,6 +81,16 @@ public static class InspectionFinder
         var locationText = listingNode.Descendants("h2")
             .Single(x => x.HasMatchingDataId("address-wrapper"))
             .InnerText;
-        return new Listing {Beds = beds, Price = price, Location = locationText};
+
+        var listingPage = listingNode.Descendants("a").Select(x => x.GetAttributeValue("href", null)).Single();
+        var pageHtml = await GetDocument(listingPage);
+        var descriptionText = pageHtml.DocumentNode.Descendants("script")
+            .Single(x =>
+                x.Id.Equals("__NEXT_DATA__")).InnerText;
+
+        var descriptionData = JsonNode.Parse(descriptionText)!["props"]!["pageProps"]!["description"]!.ToString();
+        var hasAc = Regex.IsMatch(descriptionData, @"\b(A/?C|air.?con(ditioning)?|split.?system)\b", RegexOptions.IgnoreCase);
+        
+        return new Listing {Beds = beds, Price = price, Location = locationText, AirCon = hasAc};
     }
 }
--- RentFinder/Listing.cs
+++ RentFinder/Listing.cs
@@ -4,4 +4,5 @@ public class Listing
     public string Location { get; set; }
     public decimal Price { get; set; }
     public decimal PricePerBed => Price / Beds;
+    public bool AirCon { get; set; }
 }
